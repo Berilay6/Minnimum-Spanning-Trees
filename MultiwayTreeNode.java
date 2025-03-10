8f6dbe222f115c0faa4e74b33865b4c3da6136ed
Beril <berilay005@gmail.com>
2025-03-10 20:09:22 +0300

    added comments

--- MultiwayTreeNode.java
+++ MultiwayTreeNode.java
@@ -18,10 +18,12 @@ public class MultiwayTreeNode{
     public void addChild(MultiwayTreeNode ch){
         ch.parent=this;
 
+        //case 1: new child is the first child of parent node
         if(this.firstChild == null){
             this.firstChild = ch;
         }
 
+        //case 2: new child is not the first child of parent node
         else{
             MultiwayTreeNode sibling = this.firstChild;
             while(sibling.nextSibling != null){
